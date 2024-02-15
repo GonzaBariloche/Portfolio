83123a984b3a092ce32fb4c1c892e0472d14cc96
GonzaBariloche <121523940+GonzaBariloche@users.noreply.github.com>
2024-02-15 14:47:49 +0000

    con proyecto 1

--- src/app/proyecto1/proyecto1.component.ts
+++ src/app/proyecto1/proyecto1.component.ts
@@ -1,3 +1,5 @@
+// proyecto1.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyecto1.component.css']
 })
 export class Proyecto1Component {
+  productos: string[] = ['Arroz', 'Papel Higiénico', 'Lavandina', 'Agua Mineral', 'Galletitas'];
+  bolsasDeTela: number = 0;
 
+  calcularBolsas(): void {
+    // Implementa tu lógica de cálculo de bolsas aquí
+    // Puedes usar la cantidad de productos y sus tipos para realizar el cálculo
+    // Actualiza la variable bolsasDeTela con el resultado
+  }
 }
